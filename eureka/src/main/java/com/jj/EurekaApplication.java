4542d40d468d881c51a87cec491a7b6483c8658d
jing <1873147676@qq.com>
2020-08-28 22:02:34 +0800

    1.增加eureka作为注册中心 2.改造父模块为Maven父子项目

--- eureka/src/main/java/com/jj/EurekaApplication.java
+++ eureka/src/main/java/com/jj/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.jj.education;
+package com.jj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EducationApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EducationApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
